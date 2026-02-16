33972d57778fd4ae8b88d9abb6a4a54f680e453a
rvm_danfoss <rvm@danfoss.com>
2026-02-16 15:42:11 +0530

    chore: setup for DEX-V3 deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    base: mode === 'production' ? '/DEX-V2/' : '/',
+    base: mode === 'production' ? '/DEX-V3/' : '/',
     server: {
       host: '0.0.0.0',
     },
